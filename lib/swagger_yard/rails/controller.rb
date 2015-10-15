b726b34ed0b44c71a263bf587cd4d3aad88eada0
Tony Pitale <tony.pitale@livingsocial.com>
2015-10-15 16:48:59 -0400

    add index/show endpoints to generate json for swagger-ui

---
+++ lib/swagger_yard/rails/controller.rb
@@ -0,0 +1,25 @@
+module SwaggerYard
+  module Rails
+    class Controller < ApplicationController
+      layout false
+
+      def index
+        listing = resource_listing.to_h
+        listing.merge!("basePath" => request.url) if listing["basePath"].blank?
+        render :json => listing
+      end
+
+      def show
+        declaration = resource_listing.declaration_for("/#{params[:resource]}").to_h
+
+        declaration.merge!("basePath" => request.base_url + SwaggerYard.config.api_path) if declaration["basePath"].blank?
+        render :json => declaration
+      end
+
+      private
+      def resource_listing
+        SwaggerYard::ResourceListing.new(Rails.root+'app/controllers/**/*.rb', Rails.root+'app/models/**/*.rb')
+      end
+    end
+  end
+end
