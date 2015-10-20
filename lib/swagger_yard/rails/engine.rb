6e72303a80acdfdb9de553bf140a55c1180cdc5d
Nick Sieger <nick@nicksieger.com>
2015-10-20 11:21:11 -0500

    Configure paths in engine

--- lib/swagger_yard/rails/engine.rb
+++ lib/swagger_yard/rails/engine.rb
@@ -5,6 +5,13 @@ module SwaggerYard
       initializer "swagger_yard-rails.finisher_hook" do |app|
         SwaggerYard.register_custom_yard_tags!
       end
+
+      initializer "swagger_yard-rails.paths" do |app|
+        SwaggerYard.configure do |config|
+          config.controller_path = ::Rails.root + 'app/controllers/**/*'
+          config.model_path      = ::Rails.root + 'app/models/**/*'
+        end
+      end
     end
   end
 end
