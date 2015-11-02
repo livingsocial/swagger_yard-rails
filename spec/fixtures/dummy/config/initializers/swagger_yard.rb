8db21d249808161d159034929702f2f79aae684d
Nick Sieger <nick@nicksieger.com>
2015-11-02 17:03:28 -0600

    Remove unnecessary config

--- spec/fixtures/dummy/config/initializers/swagger_yard.rb
+++ spec/fixtures/dummy/config/initializers/swagger_yard.rb
@@ -2,8 +2,6 @@ SwaggerYard.configure do |config|
   config.title = "Dummy App"
   config.description = "Dummy application for testing SwaggerYard and Rails"
   config.reload = true
-  config.swagger_version = "1.2"
   config.api_version = "1.0"
-  config.swagger_spec_base_path = "http://localhost:3000/swagger/api"
   config.api_base_path = "http://localhost:3000/api"
 end
