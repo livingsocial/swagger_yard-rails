f5661c3a84d05c26ca3ec2b243d33184f5164a02
Nick Sieger <nick@nicksieger.com>
2018-06-28 11:05:44 -0500

    Removed config

--- spec/fixtures/dummy/config/initializers/swagger_yard.rb
+++ spec/fixtures/dummy/config/initializers/swagger_yard.rb
@@ -1,7 +1,6 @@
 SwaggerYard.configure do |config|
   config.title = "Dummy App"
   config.description = "Dummy application for testing SwaggerYard and Rails"
-  config.reload = true
   config.api_version = "1.0"
   config.api_base_path = "http://localhost:3000/api"
 end
